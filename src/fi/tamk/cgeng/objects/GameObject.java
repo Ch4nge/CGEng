08b37fb507cdf8eb04b8fb73e84faa06705222ef
ch4nge <Sami.Hautamaki@cs.tamk.fi>
2017-11-19 14:08:09 +0200

    Add documentation to all object classes

--- src/fi/tamk/cgeng/objects/GameObject.java
+++ src/fi/tamk/cgeng/objects/GameObject.java
@@ -6,6 +6,14 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
+/**
+ * GameObject is object that has additional
+ * useful features compared to Sprite. Easy
+ * access to different listeneres, possibility
+ * to check collisions between GameObjects and
+ * rectangles usage: Override move method, that
+ * is being called by default in update
+ */
 public abstract class GameObject extends Sprite{
 
     private Rectangle bounds;
@@ -15,12 +23,28 @@ public abstract class GameObject extends Sprite{
     private KeyboardListener keyboardListener;
     private MousepadListener mousepadListener;
 
-
+    /**
+     * Constructor that initializes x, y, width, height and texture
+     * of GameObject. 
+     * @param x x coordinate of GameObject
+     * @param y y coordinate of GameObject
+     * @param width width of GameObject
+     * @param height height of GameObject
+     * @param image Texture of GameObject
+     */
     public GameObject(int x, int y, int width, int height, BufferedImage image){
         super(x,y,width,height,image);
         setBounds(0,0,width,height);
     }
 
+    /**
+     * Constructor that initializes x, y, width and height
+     * of GameObject
+     * @param x x coordinate of GameObject
+     * @param y y coordinate of GameObject
+     * @param width width of GameObject
+     * @param height height of GameObject
+     */
     public GameObject(int x, int y, int width, int height){
         super(x,y,width,height);
         setHeight(height);
@@ -29,63 +53,129 @@ public abstract class GameObject extends Sprite{
         setY(y);
     }
 
+    /**
+     * Functionality of gameobject(moving)
+     */
     public abstract void move();
 
+    /**
+     * update method, this is being called in 
+     * scenes update.
+     */
     public void update(){
         move();
         updateBounds(boundsX, boundsY);            
     }
 
+    /**
+     * Updates position of colliding bounds.
+     * @param boundsX x coordinate of bounds
+     * @param boundsY y coordinate of bounds
+     */
     public void updateBounds(int boundsX, int boundsY){
         bounds.setLocation(getX()+boundsX, getY()+boundsY);
     }
 
 
+    /**
+     * Set bounds to GameObject
+     * @param boundsX x coordinate of bounds
+     * @param boundsY y coordinate of bounds
+     * @param width width of bounds
+     * @param height height of bounds
+     */
     public void setBounds(int boundsX, int boundsY, int width, int height){
         setBoundsX(boundsX);
         setBoundsY(boundsY);
         bounds = new Rectangle(getX()+boundsX,getY()+boundsY,width, height);        
     }
 
+    /**
+     * Returns bounds of GameObject
+     * @return bounds
+     */
     public Rectangle getBounds(){
         return bounds;
     }
 
-
+    /**
+     * Set MousePadListener to this GameObject
+     * @param listener new MousepadListener
+     */
     public void setMousepadListener(MousepadListener listener){
         mousepadListener = listener;
     }
 
+    /**
+     * Set KeyboardListener to this GameObject
+     * @param listener new KeyboardListener
+     */
     public void setKeyboardListener(KeyboardListener listener){
         keyboardListener = listener;
     }
 
+    /**
+     * Returns KeyboardListener
+     * @return KeyboardListener
+     */
     public KeyboardListener getKeyboardListener(){
        return keyboardListener; 
     }
 
+    /**
+     * Returns MousepadListener
+     * @return MousepadListener
+     */
     public MousepadListener getMousepadListener(){
         return mousepadListener;
     }
 
+    /**
+     * Set x coordinate of bounds
+     * @param boundsX x coordinate of bounds
+     */
     public void setBoundsX(int boundsX){
         this.boundsX = boundsX;
     }
 
+    /**
+     * Set y coordinate of bounds
+     * @param boundsY y coordinate of bounds
+     */
     public void setBoundsY(int boundsY){
         this.boundsY = boundsY;
     }
+    /**
+     * Return x coordinate of bounds
+     * @return x coordinate of bounds
+     */
     public int getBoundsX(){
         return boundsX;
     }
 
+    /**
+     * Return y coordinate of bounds
+     * @return y coordinate of bounds
+     */
     public int getBoundsY(){
         return boundsY;
     }
 
+    /**
+     * Checks if this GameObject collides with other
+     * GameObject
+     * @param object GameObject we are checking
+     * @return true if collides with given GameObject
+     */
     public boolean collideWith(GameObject object){
         return bounds.intersects(object.getBounds());
     }
+    
+    /**
+     * Checks if this GameObject collides with rectangle
+     * @param rect Rectangle we are checking
+     * @return true if collides with given Rectangle
+     */
     public boolean collideWith(Rectangle rect){
         return bounds.intersects(rect);
     }
--- src/fi/tamk/cgeng/objects/Sprite.java
+++ src/fi/tamk/cgeng/objects/Sprite.java
@@ -4,16 +4,44 @@ import java.util.Optional;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
+/**
+ * Sprite is picture or shape that is 
+ * drawn on the screen
+ */
 public class Sprite{
 
-
+    /**
+     * x coordinate of Sprite
+     */
     private int x;
+    /**
+     * y coordinate of Sprite
+     */
     private int y;
+    /**
+     * width of sprite
+     */
     private int width;
+    /**
+     * height of sprite
+     */
     private int height;
 
+    /**
+     * Optional texture of sprite, all Sprites
+     * dosen't have picture.
+     */
     private Optional<BufferedImage> image;
 
+    /**
+     * Constructor that initializes all attributes of this
+     * class.
+     * @param x x coordinate of sprite
+     * @param y y coordinate of sprite
+     * @param width width of sprite
+     * @param height height of sprite
+     * @param image texture of sprite
+     */
     public Sprite(int x, int y, int width, int height, BufferedImage image){
         setHeight(height);
         setWidth(width);
@@ -22,6 +50,14 @@ public class Sprite{
         setImage(image);
     }
 
+    /**
+     * Constructor that initializes x, y, width and height
+     * of the sprite.
+     * @param x x coordinate of sprite
+     * @param y y coordinate of sprite
+     * @param width width of sprite
+     * @param height height of sprite
+     */
     public Sprite(int x, int y, int width, int height){
         setHeight(height);
         setWidth(width);
@@ -29,16 +65,33 @@ public class Sprite{
         setY(y);
     }
 
+    /**
+     * Draws Sprite to screen, draw image if it is present.
+     * @param g painted with Graphics class
+     */
     public void paint(Graphics g){
         image.ifPresent((image) -> 
             g.drawImage(image, x, y, width, height, null));
     }
 
+    /**
+     * Draws Sprite to screen, draw image if it is present.
+     * you can also change position of sprite by using
+     * xOffset and yOffset parameters, used by default in
+     * GameCam class. 
+     * @param g painted with Graphics class
+     * @param xOffset how much object is moved to left
+     * @param xOffset how much object is moved to up
+     */
     public void paint(Graphics g, int xOffset, int yOffset){
         image.ifPresent((image) -> 
             g.drawImage(image, x-xOffset, y-yOffset, width, height, null));
     }
 
+    /**
+     * Sets height of the Sprite, cant be less than 0
+     * @param height height of the sprite
+     */
     public void setHeight(int height){
         if(height < 0){
             this.height = 0;
@@ -47,10 +100,18 @@ public class Sprite{
         }
     }
 
+    /**
+     * Returns height of the spirte
+     * @return height of the sprite
+     */
     public int getHeight(){
         return height;
     }
 
+    /**
+     * Sets width of the Sprite, cant be less than 0
+     * @param width width of the sprite
+     */
     public void setWidth(int width){
         if(width < 0){
             this.width = 0;
@@ -59,30 +120,58 @@ public class Sprite{
         }
     }
 
+    /**
+     * Returns height of the spirte
+     * @return height of the sprite
+     */
     public int getWidth(){
         return width;
     }
 
+    /**
+     * Sets x coordinate of the sprite
+     * @param x x coordinate of the sprite
+     */
     public void setX(int x){
         this.x = x;
     }
 
+    /**
+     * Returns x coordinate of the spirte
+     * @return x coordinate of the sprite
+     */
     public int getX(){
         return x;
     }
 
+    /**
+     * Sets y coordinate of the sprite
+     * @param y y coordinate of the sprite
+     */
     public void setY(int y){
         this.y = y;
     }
 
+    /**
+     * Returns y coordinate of the spirte
+     * @return y coordinate of the sprite
+     */
     public int getY(){
         return y;
     }
 
+    /**
+     * Sets Image of the sprite
+     * @param image image of the sprite
+     */
     public void setImage(BufferedImage image){
         this.image = Optional.of(image);
     }
 
+    /**
+     * Returns image of the sprite
+     * @return image of the sprite
+     */
     public Optional<BufferedImage> getImage(){
         return image;
     }
